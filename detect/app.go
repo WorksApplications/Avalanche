d520d3034bf8a1b16846262cac799d4f0ed8583a
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-07-25 16:39:17 +0900

    [detect] add filtering by perf-enabled

--- detect/app.go
+++ detect/app.go
@@ -3,34 +3,29 @@ package main
 import (
 	"container/list"
 	"net/http"
-	//"fmt"
 	"log"
-	//"io/ioutil"
-	//"bytes"
 	"golang.org/x/net/html"
 	"strings"
 )
 
-var prefix string = "http://mischo.internal.worksap.com/"
+var server string = "http://mischo.internal.worksap.com/"
 
 type Subscription struct {
 	environment string
-	buks        []Node
-}
-
-type Node struct {
-	dir  string
-	apps []App
+	buks        []App
 }
 
 type App struct {
 	name string
-	pods []Pods
+	pods []Pod
 }
 
-type Pods struct {
+type Pod struct {
 	name string
-	node Node
+    link string
+    perfing bool
+	//node string
+    //namespace string
 }
 
 func findNode(z *html.Tokenizer) []string {
@@ -56,10 +51,7 @@ func findNode(z *html.Tokenizer) []string {
 	return ns
 }
 
-func findLink(site string, debug bool) ([]string, error) {
-	if debug {
-		log.Print(site)
-	}
+func findLink(site string) ([]string, error) {
 	resp, err := http.Get(site)
 	if err != nil {
 		return nil, err
@@ -87,7 +79,7 @@ func findLink(site string, debug bool) ([]string, error) {
 	return lxs, nil
 }
 
-func addPrefix(xs []string, prefix string) []string {
+func prefix(xs []string, prefix string) []string {
 	ret := make([]string, len(xs))
 	for i, x := range xs {
 		ret[i] = prefix + x
@@ -95,21 +87,56 @@ func addPrefix(xs []string, prefix string) []string {
 	return ret
 }
 
-func followLink(sites []string, linkgen func(string) string) ([]string, error) {
+func suffix(xs []string, suffix string) []string {
+	ret := make([]string, len(xs))
+	for i, x := range xs {
+		ret[i] = x + suffix
+	}
+	return ret
+}
+
+func followLink(sites []string) ([]string, error) {
 	ret := make([]string, 0)
 	for _, site := range sites {
-		link := linkgen(site)
-		next, err := findLink(link, false)
+		next, err := findLink(site)
 		if err != nil {
 			return ret, err
 		}
-		ret = append(ret, addPrefix(next, link)...)
+		ret = append(ret, prefix(next, site)...)
 	}
 	return ret, nil
 }
 
-func (s Subscription) Scan() (*App, error) {
-	resp, err := http.Get(prefix + s.environment + "/log")
+func toPods(ls []string, applink string) []Pod {
+    pods := make([]Pod, len(ls))
+    for i, l := range ls {
+        pods[i] = Pod { strings.TrimRight(l, "/"), applink + l, false }
+    }
+    return pods
+}
+
+func isNotFound(resp *http.Response) bool {
+	z := html.NewTokenizer(resp.Body)
+	for {
+		tt := z.Next()
+		switch {
+		case tt == html.ErrorToken:
+            return false
+		case tt == html.StartTagToken:
+			t := z.Token()
+			if t.Data == "title" {
+                z.Next()
+                if "404 Not Found" == string(z.Text()) {
+                    return true
+                }
+			}
+		}
+	}
+
+}
+
+func (s Subscription) Scan() ([]App, error) {
+	resp, err := http.Get(server + s.environment + "/log")
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
@@ -118,22 +145,55 @@ func (s Subscription) Scan() (*App, error) {
 	defer resp.Body.Close()
 	z := html.NewTokenizer(resp.Body)
 	log.Print("[Scan] " + s.environment)
-	ns := addPrefix(findNode(z), prefix+s.environment+"/log/")
+	ns := suffix(prefix(findNode(z), server+s.environment+"/log/"), "msa/")
 
 	/* Here it generates links like /acdev/log/kubernetes-10.207.5.30/msa/acdev/ */
-	logd, _ := followLink(ns, func(l string) string { return l + "msa/" })
+	logd, err := followLink(ns)
+    if err != nil {
+        log.Print(s.environment + "is not mounted")
+        return nil, err
+    }
 
 	/* eg: /acdev/log/kubernetes-10.207.5.30/msa/acdev/develop/ */
-	ls, _ := followLink(logd, func(l string) string { return l })
+	log2, err := followLink(logd)
+    if err != nil {
+        log.Print(s.environment + "is closed")
+        return nil, err
+    }
 
-	for _, l := range ls {
-		log.Print(l)
-		apps, _ := findLink(l, false)
-		for _, a := range apps {
-			log.Print(a)
+    mapps := make(map[string][]Pod)
+	for _, l := range log2 {
+		ap, _ := findLink(l)
+		for _, name := range ap {
+            p, _ := findLink(l + name)
+            pods := toPods(p, l + name)
+            found, is := mapps[name]
+            if is {
+                pods = append(found, pods...)
+            }
+            mapps[name] = pods
 		}
 	}
-	return nil, nil
+
+    /* Check those pods are perf-enabled */
+    apps := make([]App, 0)
+    for name, pods := range mapps {
+        if strings.HasPrefix(name, "batch") {
+            continue
+        }
+        npds := make([]Pod, 0)
+        for _, pod := range pods {
+            con, err := http.Get(pod.link + "perf/")
+            defer con.Body.Close()
+            if err == nil && !isNotFound(con) {
+                npds = append(npds, pod)
+            }
+        }
+
+        apps = append(apps, App{ name, npds })
+    }
+
+	return apps, nil
 }
 
 func main() {
@@ -141,8 +201,9 @@ func main() {
 	s := Subscription{"acdev", nil}
 	sxs.PushFront(s)
 
-	log.SetPrefix("collect:\t")
-	s.Scan()
+	log.SetPrefix("detect:\t")
+    apps, _ := s.Scan()
+    log.Print(apps)
 	//http.HandleFunc("/subscription", s.Poll)
 	//log.Fatal(http.ListenAndServe(":8080", nil))
 }
