894e6f2fbcdf0a5c59cd5e56ae71aa009e215d43
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-07-26 18:08:26 +0900

    [detect] first post handler

--- cmd/detect/app.go
+++ cmd/detect/app.go
@@ -5,14 +5,15 @@ import (
 	"net/http"
 	//"strings"
 	"../../pkg/crowler"
-	"../../pkg/detectHandler"
+	"../../pkg/detectService"
 )
 
 func main() {
 	log.SetPrefix("detect:\t")
 	x := detectService.HandlerClosure{make(chan *crowl.ScannerRequest)}
-	go crowl.ScheduleScan(x.ch)
+	go crowl.ScheduleScan(x.Ch)
 	//log.Print(apps)
-	http.HandleFunc("/subscription/", x.runner)
+	http.HandleFunc("/subscription/", x.Runner)
+	http.HandleFunc("/subscription", x.Runner)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- pkg/detectService/pkg.go
+++ pkg/detectService/pkg.go
@@ -1,17 +1,55 @@
 package detectService
 
 import (
-	"log"
 	"net/http"
+    "log"
 	//"strings"
-	"./crowler"
+	"../crowler"
+    "time"
+    "fmt"
+    "bytes"
 )
 
 type HandlerClosure struct {
-    ch chan *crowl.ScannerRequest
+    Ch chan *crowl.ScannerRequest
 }
 
-func (s handlerClosure) Runner(res http.ResponseWriter, req *http.Request) {
+func subscribe(res http.ResponseWriter, req *http.Request, ch chan *crowl.ScannerRequest) {
+    if req.URL.Path != "/subscription" {
+        res.WriteHeader(http.StatusNotFound)
+        return
+    }
+    buf := new(bytes.Buffer)
+    defer req.Body.Close()
+    _, e := buf.ReadFrom(req.Body)
+    if e != nil {
+        res.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    sub := crowl.Subscription{buf.String(), nil}
+    sreq := crowl.ScannerRequest{crowl.ADD, &sub}
+
+    t := time.NewTimer(20 * time.Second)
+
+    select {
+    case <-t.C:
+        res.WriteHeader(http.StatusRequestTimeout)
+    case ch <- &sreq:
+        res.WriteHeader(http.StatusOK)
+    }
+
+
+    fmt.Fprintf(res, "%s", buf)
 }
 
+func (s HandlerClosure) Runner(res http.ResponseWriter, req *http.Request) {
+    log.Printf("%s %s", req.Method, req.URL.Path)
+    switch req.Method {
+    case "GET":
+
+    case "POST":
+        subscribe(res, req, s.Ch)
+    case "DELETE":
+    }
+}
 
