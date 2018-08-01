47854e57d0e67d8f5c557be26c7d25b3506ce792
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-08-01 18:25:33 +0900

    [collect] start to implement detect-pulling

--- cmd/collect/server.go
+++ cmd/collect/server.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
+	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/models"
 	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/restapi"
 	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/restapi/operations"
-	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/models"
 	loads "github.com/go-openapi/loads"
 	middleware "github.com/go-openapi/runtime/middleware"
 
@@ -19,7 +20,8 @@ import (
 )
 
 type ServerCtx struct {
-	Db *sql.DB
+	Db     *sql.DB
+	detect string /* detect address */
 }
 
 func (s *ServerCtx) getAppsHandler(_ operations.GetAppsParams) middleware.Responder {
@@ -29,54 +31,65 @@ func (s *ServerCtx) getAppsHandler(_ operations.GetAppsParams) middleware.Respon
 
 func (s *ServerCtx) describeAppHandler(params operations.DescribeAppParams) middleware.Responder {
 	body := app.Describe(s.Db, &params.Appid)
-    if body == nil {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
+	if body == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
 	return operations.NewDescribeAppOK().WithPayload(body)
 }
 
 func (s *ServerCtx) getEnvironmentsHandler(params operations.GetEnvironmentsParams) middleware.Responder {
-    app := app.Describe(s.Db, &params.Appid)
-    if app == nil {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    lays := layout.OfApp(*app.ID, s.Db)
-    if len(lays) == 0 {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    body := make([]*models.Environment, len(lays))
-    for _, l := range lays {
-        body = append(body, environ.FromLayout(s.Db, l))
-    }
+	app := app.Describe(s.Db, &params.Appid)
+	if app == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfApp(*app.ID, s.Db)
+	if len(lays) == 0 {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := make([]*models.Environment, len(lays))
+	for _, l := range lays {
+		body = append(body, environ.FromLayout(s.Db, l))
+	}
 	return operations.NewGetEnvironmentsOK().WithPayload(body)
 }
 
 func (s *ServerCtx) describeEnvironmentHandler(params operations.DescribeEnvironmentParams) middleware.Responder {
-    app := app.Describe(s.Db, &params.Appid)
-    env := environ.Get(s.Db, &params.Environment)
-    if app == nil || env == nil {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    lays := layout.OfBoth(*app.ID, *env.ID, s.Db)
-    if len(lays) == 0 {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    body := environ.FromLayout(s.Db, lays[0])
+	app := app.Describe(s.Db, &params.Appid)
+	env := environ.Get(s.Db, &params.Environment)
+	if app == nil || env == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfBoth(*app.ID, *env.ID, s.Db)
+	if len(lays) == 0 {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := environ.FromLayout(s.Db, lays[0])
 	return operations.NewDescribeEnvironmentOK().WithPayload(body)
 }
 
 func (s *ServerCtx) getPodsHandler(params operations.GetPodsParams) middleware.Responder {
-    app := app.Describe(s.Db, &params.Appid)
-    env := environ.Get(s.Db, &params.Environment)
-    if app == nil || env == nil {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    lays := layout.OfBoth(*app.ID, *env.ID, s.Db)
-    if len(lays) == 0 {
-        return operations.NewDescribeAppDefault(404).WithPayload(nil)
-    }
-    body := pod.FromLayout(s.Db, lays[0])
+	app := app.Describe(s.Db, &params.Appid)
+	env := environ.Get(s.Db, &params.Environment)
+	if app == nil || env == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfBoth(*app.ID, *env.ID, s.Db)
+	if len(lays) == 0 {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := pod.FromLayout(s.Db, lays[0])
 	return operations.NewGetPodsOK().WithPayload(*body)
+}
+
+func (s *ServerCtx) pollPodInfo() {
+	/* If the retrieving schedule is invoked by detect, the place to post new data is unknown on testing. */
+	ticker := time.NewTicker(2 * time.Minute)
+	go func() {
+		for t := range ticker.C {
+			log.Printf("start to pull pods' information from %s|%s", s.detect, t)
+			//pullDetect(s.Db)
+		}
+	}()
 }
 
 func (s *ServerCtx) initHandle() {
@@ -104,20 +117,21 @@ func main() {
 
 	api := operations.NewCollectAPI(swaggerSpec)
 
-    init := flag.Bool("init", false, "Initialize?")
-    dbconf := flag.String("db", "example:example@localhost", "DB connexion")
-    port := flag.Int("port", 4981, "Port for this server")
+	init := flag.Bool("init", false, "Initialize?")
+	dbconf := flag.String("db", "example:example@localhost", "DB connexion")
+	port := flag.Int("port", 4981, "Port for this server")
+	detect := flag.String("detect", "http://localhost:8081/", "\"detect\" service address")
 
-    flag.Parse()
-    args := flag.Args()
-    log.Println(args)
+	flag.Parse()
+	args := flag.Args()
+	log.Println(args)
 
 	server := restapi.NewServer(api)
-    server.Port = *port
+	server.Port = *port
 	defer server.Shutdown()
 
-    db := establishDBConn(*dbconf)
-	ctx := ServerCtx{db}
+	db := establishDBConn(*dbconf)
+	ctx := ServerCtx{db, *detect}
 
 	api.GetAppsHandler = operations.GetAppsHandlerFunc(ctx.getAppsHandler)
 	api.DescribeAppHandler = operations.DescribeAppHandlerFunc(ctx.describeAppHandler)
