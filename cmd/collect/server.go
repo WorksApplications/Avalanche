ba16f655ff1289164c08fa53cd4ffc2d5b842cdb
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-08-03 17:03:22 +0900

    [collect] separate serverCtx file

--- cmd/collect/server.go
+++ cmd/collect/server.go
@@ -1,164 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"time"
 
-	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/models"
 	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/restapi"
 	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/restapi/operations"
 	loads "github.com/go-openapi/loads"
-	middleware "github.com/go-openapi/runtime/middleware"
+
+	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/serverCtx"
 
 	"database/sql"
-	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/apps"
-	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/environments"
-	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/layout"
-	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/pod"
-
-	"git.paas.workslan/resource_optimization/dynamic_analysis/pkg/detect"
 	_ "github.com/go-sql-driver/mysql"
 )
-
-type ServerCtx struct {
-	Db     *sql.DB
-	detect string /* detect address */
-}
-
-func (s *ServerCtx) healthzHandler(_ operations.HealthzParams) middleware.Responder {
-	return operations.NewHealthzOK().WithPayload("Vaer sa godt")
-}
-
-func (s *ServerCtx) getAppsHandler(_ operations.GetAppsParams) middleware.Responder {
-	body := app.ListAll(s.Db)
-	return operations.NewGetAppsOK().WithPayload(body)
-}
-
-func (s *ServerCtx) describeAppHandler(params operations.DescribeAppParams) middleware.Responder {
-	body := app.Describe(s.Db, &params.Appid)
-	if body == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	return operations.NewDescribeAppOK().WithPayload(body)
-}
-
-func (s *ServerCtx) getEnvironmentsHandler(params operations.GetEnvironmentsParams) middleware.Responder {
-	app := app.Describe(s.Db, &params.Appid)
-	if app == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	lays := layout.OfApp(s.Db, *app.ID)
-	if len(lays) == 0 {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	body := make([]*models.Environment, len(lays))
-	for _, l := range lays {
-		body = append(body, environ.FromLayout(s.Db, l))
-	}
-	return operations.NewGetEnvironmentsOK().WithPayload(body)
-}
-
-func (s *ServerCtx) describeEnvironmentHandler(params operations.DescribeEnvironmentParams) middleware.Responder {
-	app := app.Describe(s.Db, &params.Appid)
-	env := environ.Get(s.Db, &params.Environment)
-	if app == nil || env == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	lays := layout.OfBoth(s.Db, *app.ID, *env.ID)
-	if lays == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	body := environ.FromLayout(s.Db, lays)
-	return operations.NewDescribeEnvironmentOK().WithPayload(body)
-}
-
-func (s *ServerCtx) getPodsHandler(params operations.GetPodsParams) middleware.Responder {
-	app := app.Describe(s.Db, &params.Appid)
-	env := environ.Get(s.Db, &params.Environment)
-	if app == nil || env == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	lays := layout.OfBoth(s.Db, *app.ID, *env.ID)
-	if lays == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	body := pod.FromLayout(s.Db, lays)
-	return operations.NewGetPodsOK().WithPayload(body)
-}
-
-func (s *ServerCtx) describePodHandler(params operations.DescribePodParams) middleware.Responder {
-	app := app.Describe(s.Db, &params.Appid)
-	env := environ.Get(s.Db, &params.Environment)
-	if app == nil || env == nil {
-		return operations.NewDescribeAppDefault(404).WithPayload(nil)
-	}
-	body := pod.Get(s.Db, &params.Pod, env.ID, app.ID)
-	return operations.NewDescribePodOK().WithPayload(body)
-}
-
-func (s *ServerCtx) pull() {
-	log.Printf("start to pull pods' information from %s", s.detect)
-	r, err := http.Get(s.detect + "/subscription/")
-	d, er2 := ioutil.ReadAll(r.Body)
-	if err != nil || er2 != nil {
-		log.Println("Poll failed!")
-		return
-	}
-	defer r.Body.Close()
-
-	/* BUG XXX don't update existing info now! It is a bug XXX */
-	var p []detect.Subscription
-	err = json.Unmarshal(d, &p)
-	if err != nil || er2 != nil {
-		log.Println("Unmarshal failed!", err, er2)
-		log.Printf("res: %+v", p)
-		return
-	}
-	for _, e := range p {
-		recursiveInsert(s.Db, &e)
-	}
-}
-
-func recursiveInsert(db *sql.DB, p *detect.Subscription) {
-	en := environ.Assign(db, &p.Env)
-	for _, a := range p.Apps {
-		an := app.Assign(db, &a.Name, &a.Seen)
-		l := layout.Assign(db, *en.ID, *an.ID)
-		for _, p := range a.Pods {
-			pod.Assign(db, &p.Name, *en.ID, *an.ID, l.Id)
-		}
-	}
-}
-
-func (s *ServerCtx) pollPodInfo() {
-	/* If the retrieving schedule is invoked by detect, the place to post new data is unknown on testing. */
-	t := time.NewTicker(1 * time.Minute)
-	once := make(chan int, 1)
-	once <- 1
-	go func() {
-		for {
-			select {
-			case <-once:
-				close(once)
-				once = nil
-				s.pull()
-			case <-t.C:
-				s.pull()
-			}
-		}
-	}()
-}
-
-func (s *ServerCtx) initHandle() {
-	environ.InitTable(s.Db)
-	layout.InitTable(s.Db)
-	pod.InitTable(s.Db)
-	app.InitTable(s.Db)
-}
 
 func establishDBConn(dn string) *sql.DB {
 	db, err := sql.Open("mysql", dn)
@@ -192,25 +46,25 @@ func main() {
 	defer server.Shutdown()
 
 	db := establishDBConn(*dbconf)
-	ctx := ServerCtx{db, *detect}
+	ctx := serverCtx.ServerCtx{db, *detect}
 
 	if *init {
-		ctx.initHandle()
+		ctx.InitHandle()
 		log.Fatal("Initialize end")
 	}
 
-	ctx.pollPodInfo()
+	ctx.PollPodInfo()
 
-	api.GetAppsHandler = operations.GetAppsHandlerFunc(ctx.getAppsHandler)
-	api.DescribeAppHandler = operations.DescribeAppHandlerFunc(ctx.describeAppHandler)
+	api.GetAppsHandler = operations.GetAppsHandlerFunc(ctx.GetAppsHandler)
+	api.DescribeAppHandler = operations.DescribeAppHandlerFunc(ctx.DescribeAppHandler)
 
-	api.GetEnvironmentsHandler = operations.GetEnvironmentsHandlerFunc(ctx.getEnvironmentsHandler)
-	api.DescribeEnvironmentHandler = operations.DescribeEnvironmentHandlerFunc(ctx.describeEnvironmentHandler)
+	api.GetEnvironmentsHandler = operations.GetEnvironmentsHandlerFunc(ctx.GetEnvironmentsHandler)
+	api.DescribeEnvironmentHandler = operations.DescribeEnvironmentHandlerFunc(ctx.DescribeEnvironmentHandler)
 
-	api.GetPodsHandler = operations.GetPodsHandlerFunc(ctx.getPodsHandler)
-	api.DescribePodHandler = operations.DescribePodHandlerFunc(ctx.describePodHandler)
+	api.GetPodsHandler = operations.GetPodsHandlerFunc(ctx.GetPodsHandler)
+	api.DescribePodHandler = operations.DescribePodHandlerFunc(ctx.DescribePodHandler)
 
-	api.HealthzHandler = operations.HealthzHandlerFunc(ctx.healthzHandler)
+	api.HealthzHandler = operations.HealthzHandlerFunc(ctx.HealthzHandler)
 
 	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
---
+++ cmd/collect/serverCtx/serverCtx.go
@@ -0,0 +1,159 @@
+package serverCtx
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/models"
+	"git.paas.workslan/resource_optimization/dynamic_analysis/generated_files/restapi/operations"
+	middleware "github.com/go-openapi/runtime/middleware"
+
+	"database/sql"
+	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/apps"
+	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/environments"
+	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/layout"
+	"git.paas.workslan/resource_optimization/dynamic_analysis/cmd/collect/pod"
+
+	"git.paas.workslan/resource_optimization/dynamic_analysis/pkg/detect"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type ServerCtx struct {
+	Db     *sql.DB
+	Detect string /* detect address */
+}
+
+func (s *ServerCtx) HealthzHandler(_ operations.HealthzParams) middleware.Responder {
+	return operations.NewHealthzOK().WithPayload("Vaer sa godt")
+}
+
+func (s *ServerCtx) GetAppsHandler(_ operations.GetAppsParams) middleware.Responder {
+	body := app.ListAll(s.Db)
+	return operations.NewGetAppsOK().WithPayload(body)
+}
+
+func (s *ServerCtx) DescribeAppHandler(params operations.DescribeAppParams) middleware.Responder {
+	body := app.Describe(s.Db, &params.Appid)
+	if body == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	return operations.NewDescribeAppOK().WithPayload(body)
+}
+
+func (s *ServerCtx) GetEnvironmentsHandler(params operations.GetEnvironmentsParams) middleware.Responder {
+	app := app.Describe(s.Db, &params.Appid)
+	if app == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfApp(s.Db, *app.ID)
+	if len(lays) == 0 {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := make([]*models.Environment, len(lays))
+	for _, l := range lays {
+		body = append(body, environ.FromLayout(s.Db, l))
+	}
+	return operations.NewGetEnvironmentsOK().WithPayload(body)
+}
+
+func (s *ServerCtx) DescribeEnvironmentHandler(params operations.DescribeEnvironmentParams) middleware.Responder {
+	app := app.Describe(s.Db, &params.Appid)
+	env := environ.Get(s.Db, &params.Environment)
+	if app == nil || env == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfBoth(s.Db, *app.ID, *env.ID)
+	if lays == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := environ.FromLayout(s.Db, lays)
+	return operations.NewDescribeEnvironmentOK().WithPayload(body)
+}
+
+func (s *ServerCtx) GetPodsHandler(params operations.GetPodsParams) middleware.Responder {
+	app := app.Describe(s.Db, &params.Appid)
+	env := environ.Get(s.Db, &params.Environment)
+	if app == nil || env == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	lays := layout.OfBoth(s.Db, *app.ID, *env.ID)
+	if lays == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := pod.FromLayout(s.Db, lays)
+	return operations.NewGetPodsOK().WithPayload(body)
+}
+
+func (s *ServerCtx) DescribePodHandler(params operations.DescribePodParams) middleware.Responder {
+	app := app.Describe(s.Db, &params.Appid)
+	env := environ.Get(s.Db, &params.Environment)
+	if app == nil || env == nil {
+		return operations.NewDescribeAppDefault(404).WithPayload(nil)
+	}
+	body := pod.Get(s.Db, &params.Pod, env.ID, app.ID)
+	return operations.NewDescribePodOK().WithPayload(body)
+}
+
+func (s *ServerCtx) pull() {
+	log.Printf("start to pull pods' information from %s", s.Detect)
+	r, err := http.Get(s.Detect + "/subscription/")
+	d, er2 := ioutil.ReadAll(r.Body)
+	if err != nil || er2 != nil {
+		log.Println("Poll failed!")
+		return
+	}
+	defer r.Body.Close()
+
+    /* BUG XXX don't update existing info now! It is a bug XXX */
+	var p []detect.Subscription
+	err = json.Unmarshal(d, &p)
+	if err != nil || er2 != nil {
+		log.Println("Unmarshal failed!", err, er2)
+		log.Printf("res: %+v", p)
+		return
+	}
+	for _, e := range p {
+		recursiveInsert(s.Db, &e)
+	}
+}
+
+func (s *ServerCtx) PollPodInfo() {
+	/* If the retrieving schedule is invoked by detect, the place to post new data is unknown on testing. */
+	t := time.NewTicker(1 * time.Minute)
+	once := make(chan int, 1)
+	once <- 1
+	go func() {
+		for {
+			select {
+			case <-once:
+				close(once)
+				once = nil
+				s.pull()
+			case <-t.C:
+				s.pull()
+			}
+		}
+	}()
+}
+
+func recursiveInsert(db *sql.DB, p *detect.Subscription) {
+	en := environ.Assign(db, &p.Env)
+	for _, a := range p.Apps {
+		an := app.Assign(db, &a.Name, &a.Seen)
+		l := layout.Assign(db, *en.ID, *an.ID)
+		for _, p := range a.Pods {
+			pod.Assign(db, &p.Name, *en.ID, *an.ID, l.Id)
+		}
+	}
+}
+
+func (s *ServerCtx) InitHandle() {
+	environ.InitTable(s.Db)
+	layout.InitTable(s.Db)
+	pod.InitTable(s.Db)
+	app.InitTable(s.Db)
+}
+
