3183975df26a70f1857dfa2843a42d9aee7d8927
Takahiro Okada <okada_tak@worksap.co.jp>
2018-09-25 07:16:57 +0900

    Move application context selector.

--- front/src/containers/NavigationView.tsx
+++ front/src/containers/NavigationView.tsx
@@ -1,80 +1,13 @@
-import { Component, FunctionalComponent, h } from "preact";
-import { connect } from "preact-redux";
+import { Component, h } from "preact";
 // tslint:disable-next-line:no-submodule-imports
 import { Link } from "preact-router/match";
-import * as qs from "querystring";
-import { bindActionCreators, Dispatch } from "redux";
-import {
-  getEnvironmentsOfApp,
-  selectApp,
-  selectEnv,
-  selectPod
-} from "../actions";
-import AppSelector from "../components/AppSelector";
-import { APP_NAME } from "../constants";
-import { IApplicationState } from "../store";
 // @ts-ignore
 import styles from "./NavigationView.scss";
 
-interface IStateProps {
-  applicationName: string | null;
-  applications: string[];
-}
-
-interface IDispatchProps {
-  selectApp: typeof selectApp;
-  getEnvironmentsOfApp: typeof getEnvironmentsOfApp;
-  selectEnv: typeof selectEnv;
-  selectPod: typeof selectPod;
-}
-
-const mapStateToProps: (state: IApplicationState) => IStateProps = state => ({
-  applicationName: state.analysisData.applicationName,
-  applications: state.analysisData.applications
-});
-
-const mapDispatchToProps: (dispatch: Dispatch) => IDispatchProps = dispatch =>
-  bindActionCreators(
-    { selectApp, getEnvironmentsOfApp, selectEnv, selectPod },
-    dispatch
-  );
-
-// @ts-ignore
-@connect(
-  mapStateToProps,
-  mapDispatchToProps
-)
-class NavigationView extends Component<IStateProps & IDispatchProps> {
-  public componentWillMount() {
-    // get app & env from query in url
-    const requested = qs.parse(window.location.search.substring(1));
-    if (
-      "app" in requested &&
-      typeof requested.app === "string" &&
-      requested.app
-    ) {
-      this.changeApp(requested.app, true);
-    }
-  }
-
+class NavigationView extends Component {
   public render() {
-    const showingData = this.props.applications.map(x => ({
-      label: x,
-      value: x
-    }));
     return (
       <div className={styles.wrap}>
-        <div className={styles.appContext}>
-          <div className={styles.label}>Application Context</div>
-          <div className={styles.selector}>
-            <AppSelector
-              options={showingData}
-              selectedValue={this.props.applicationName}
-              onValueChanged={this.onAppChanged.bind(this)}
-              placeholder="Select Application"
-            />
-          </div>
-        </div>
         <div className={styles.viewList}>
           <Link
             activeClassName={styles.selected}
@@ -96,27 +29,6 @@ class NavigationView extends Component<IStateProps & IDispatchProps> {
       </div>
     );
   }
-
-  private onAppChanged(app: string) {
-    this.changeApp(app);
-  }
-
-  private changeApp(app: string, isInizalizing = false) {
-    this.props.selectApp({ appName: app });
-    this.props.getEnvironmentsOfApp(app);
-    this.props.selectEnv({ envName: null }); // unselect
-    this.props.selectPod({ podName: null }); // unselect
-
-    // set app to query in url
-    const requested = qs.parse(window.location.search.substring(1));
-    const newQuery = { ...requested };
-    newQuery.app = app;
-    if (!isInizalizing && "env" in newQuery) {
-      delete newQuery.env;
-    }
-    // current browser does not support 2nd argument :yaomin:
-    history.pushState({}, `${APP_NAME} | ${app}`, "?" + qs.stringify(newQuery));
-  }
 }
 
-export default (NavigationView as any) as FunctionalComponent;
+export default NavigationView;
--- front/src/containers/SnapshotsView.tsx
+++ front/src/containers/SnapshotsView.tsx
@@ -2,7 +2,13 @@ import { Component, FunctionalComponent, h } from "preact";
 import { connect } from "preact-redux";
 import * as qs from "querystring";
 import { bindActionCreators, Dispatch } from "redux";
-import { selectEnv, selectPod } from "../actions";
+import {
+  getEnvironmentsOfApp,
+  selectApp,
+  selectEnv,
+  selectPod
+} from "../actions";
+import AppSelector from "../components/AppSelector";
 import SnapshotFilter from "../components/SnapshotFilter";
 import SnapshotList, { IRowData } from "../components/SnapshotList";
 import { APP_NAME } from "../constants";
@@ -17,6 +23,7 @@ import styles from "./SnapshotsView.scss";
 
 interface IStateProps {
   appName: string | null;
+  apps: string[];
   environments: { [appName: string]: IEnvironmentInfo };
   filteringEnvironment: string | null;
   filteringPod: string | null;
@@ -25,6 +32,8 @@ interface IStateProps {
 }
 
 interface IDispatchProps {
+  selectApp: typeof selectApp;
+  getEnvironmentsOfApp: typeof getEnvironmentsOfApp;
   selectEnv: typeof selectEnv;
   selectPod: typeof selectPod;
 }
@@ -39,6 +48,7 @@ const mapStateToProps: (state: IApplicationState) => IStateProps = state => {
   );
   return {
     appName: state.analysisData.applicationName,
+    apps: state.analysisData.applications,
     environments: state.analysisData.environments,
     filteringEnvironment: state.analysisData.selectedEnvironment,
     filteringPod: state.analysisData.selectedPod,
@@ -52,7 +62,10 @@ const mapStateToProps: (state: IApplicationState) => IStateProps = state => {
 };
 
 const mapDispatchToProps: (dispatch: Dispatch) => IDispatchProps = dispatch =>
-  bindActionCreators({ selectEnv, selectPod }, dispatch);
+  bindActionCreators(
+    { selectApp, getEnvironmentsOfApp, selectEnv, selectPod },
+    dispatch
+  );
 
 // @ts-ignore
 @connect(
@@ -66,32 +79,42 @@ class SnapshotsView extends Component<IStateProps & IDispatchProps> {
     if (
       "app" in requested &&
       typeof requested.app === "string" &&
-      requested.app &&
-      "env" in requested &&
-      typeof requested.env === "string" &&
-      requested.env
+      requested.app
     ) {
-      this.changeEnvironment(requested.env);
+      this.changeApp(requested.app, true);
+
+      if (
+        "env" in requested &&
+        typeof requested.env === "string" &&
+        requested.env
+      ) {
+        this.changeEnvironment(requested.env);
+      }
     }
   }
 
   public render() {
+    const showingApps = this.props.apps.map(x => ({
+      label: x,
+      value: x
+    }));
+
     const environmentNames: string[] = Object.keys(
       this.props.environments || {}
     );
-    const podNames: string[] = this.props.pods.filter(
-        x => x.snapshots && x.snapshots.length != 0
-    ).map(x => x.name);
+    const podNames: string[] = this.props.pods
+      .filter(x => x.snapshots && x.snapshots.length !== 0)
+      .map(x => x.name);
 
     const envFilterData = environmentNames.map(x => ({ label: x, value: x }));
     const podFilterData = podNames.map(x => ({ label: x, value: x }));
 
     let emptyMessage =
       this.props.snapshots.length > 0 ? "Please select environment" : "No Data";
-    let showingData: IRowData[] = [];
+    let showingSnapshots: IRowData[] = [];
 
     if (this.props.snapshots.length > 0) {
-      showingData = this.props.snapshots.map(x => ({
+      showingSnapshots = this.props.snapshots.map(x => ({
         uuid: x.uuid,
         name: x.name || "Unknown",
         environment: x.environment || "Unknown",
@@ -102,16 +125,16 @@ class SnapshotsView extends Component<IStateProps & IDispatchProps> {
         isReady: false // TODO
       }));
       if (this.props.filteringEnvironment) {
-          showingData = showingData.filter(
-            x => x.environment === this.props.filteringEnvironment
-          );
+        showingSnapshots = showingSnapshots.filter(
+          x => x.environment === this.props.filteringEnvironment
+        );
       }
       if (this.props.filteringPod) {
-        showingData = showingData.filter(
+        showingSnapshots = showingSnapshots.filter(
           x => x.pod === this.props.filteringPod
         );
       }
-      if (showingData.length === 0) {
+      if (showingSnapshots.length === 0) {
         emptyMessage = "No Data";
       }
     }
@@ -119,36 +142,65 @@ class SnapshotsView extends Component<IStateProps & IDispatchProps> {
     // TODO fix cell width value
 
     return (
-      <div
-        className={[
-          styles.wrap,
-          !this.props.appName ? styles.waitForAppSelect : undefined
-        ].join(" ")}
-      >
-        <div className={styles.environmentSelector}>
-          <SnapshotFilter
-            options={envFilterData}
-            selectedValue={this.props.filteringEnvironment}
-            onValueChanged={this.onEnvironmentChanged.bind(this)}
-            placeholder="Select environment"
-            disabled={!this.props.appName}
-          />
+      <div className={styles.wrap}>
+        <div className={styles.appContext}>
+          <div className={styles.label}>Application Context</div>
+          <div className={styles.selector}>
+            <AppSelector
+              options={showingApps}
+              selectedValue={this.props.appName}
+              onValueChanged={this.onAppChanged.bind(this)}
+              placeholder="Select Application"
+            />
+          </div>
         </div>
-        <div className={styles.podSelector}>
-          <SnapshotFilter
-            options={podFilterData}
-            selectedValue={this.props.filteringPod}
-            onValueChanged={this.onPodChanged.bind(this)}
-            placeholder="Select pod name"
-            unselectOptionLabel="Deselect"
-            disabled={!this.props.filteringEnvironment}
-          />
-        </div>
-        <div className={styles.snapshotList}>
-          <SnapshotList rows={showingData} emptyMessage={emptyMessage} />
+        <div className={styles.innerWrap}>
+          <div className={styles.environmentSelector}>
+            <SnapshotFilter
+              options={envFilterData}
+              selectedValue={this.props.filteringEnvironment}
+              onValueChanged={this.onEnvironmentChanged.bind(this)}
+              placeholder="Select environment"
+              disabled={!this.props.appName}
+            />
+          </div>
+          <div className={styles.podSelector}>
+            <SnapshotFilter
+              options={podFilterData}
+              selectedValue={this.props.filteringPod}
+              onValueChanged={this.onPodChanged.bind(this)}
+              placeholder="Select pod name"
+              unselectOptionLabel="Deselect"
+              disabled={!this.props.filteringEnvironment}
+            />
+          </div>
+          <div className={styles.snapshotList}>
+            <SnapshotList rows={showingSnapshots} emptyMessage={emptyMessage} />
+          </div>
         </div>
       </div>
     );
+  }
+
+  private onAppChanged(app: string) {
+    this.changeApp(app);
+  }
+
+  private changeApp(app: string, isInitializing = false) {
+    this.props.selectApp({ appName: app });
+    this.props.getEnvironmentsOfApp(app);
+    this.props.selectEnv({ envName: null }); // unselect
+    this.props.selectPod({ podName: null }); // unselect
+
+    // set app to query in url
+    const requested = qs.parse(window.location.search.substring(1));
+    const newQuery = { ...requested };
+    newQuery.app = app;
+    if (!isInitializing && "env" in newQuery) {
+      delete newQuery.env;
+    }
+    // current browser does not support 2nd argument :yaomin:
+    history.pushState({}, `${APP_NAME} | ${app}`, "?" + qs.stringify(newQuery));
   }
 
   private onEnvironmentChanged(env: string) {
