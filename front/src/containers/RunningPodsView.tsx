17ce100b99e35cd3a829f6e049b3469eab270a73
Takahiro Okada <okada_tak@worksap.co.jp>
2018-10-29 18:42:15 +0900

    Change pod filtering behavior.

--- front/src/containers/RunningPodsView.tsx
+++ front/src/containers/RunningPodsView.tsx
@@ -92,7 +92,7 @@ export class RunningPodsView extends React.Component<IProps, IState> {
 
   public render() {
     const podInfo: IPodCardListData = this.props.pods
-      .filter(p => p.name.startsWith(this.state.filteringValue))
+      .filter(p => p.name.includes(this.state.filteringValue))
       .map(p => ({
         id: (p.id || "").toString(),
         name: p.name,
