6922c1acb81c12eb61b57393c07c79b8e9628842
Takahiro Okada <okada_tak@worksap.co.jp>
2018-11-06 18:27:06 +0900

    refactor: split SnapshotList into 3 pieces

--- front/src/components/SnapshotList/SnapshotList.tsx
+++ front/src/components/SnapshotList/SnapshotList.tsx
@@ -1,3 +1,4 @@
+// tslint:disable:max-classes-per-file
 import * as React from "react";
 import Link from "../Link";
 import styles from "./SnapshotList.scss";
@@ -8,6 +9,38 @@ export interface IData {
   podName: string;
   createdAt?: Date;
   link: string;
+}
+
+// This child component is tightly coupled as a table row element
+export class SnapshotItem extends React.Component<IData> {
+  public render() {
+    return (
+      <tbody data-testid="snapshot">
+        <tr>
+          <td>{this.props.uuid}</td>
+          <td>{this.props.podName}</td>
+          <td>{this.props.environment}</td>
+          <td data-testid="snapshot-date">
+            {(this.props.createdAt && this.props.createdAt.toLocaleString()) ||
+              "Unknown"}
+          </td>
+          <td>
+            <Link href={this.props.link}>Framescope</Link>
+          </td>
+        </tr>
+      </tbody>
+    );
+  }
+}
+
+export function Empty(props: { emptyMessage?: string }) {
+  return (
+    <tbody>
+      <tr className={styles.empty} data-testid="empty-message">
+        <td colSpan={5}>{props.emptyMessage}</td>
+      </tr>
+    </tbody>
+  );
 }
 
 export interface IProperty {
@@ -30,26 +63,10 @@ export class SnapshotList extends React.Component<IProperty> {
             </tr>
           </thead>
           {this.props.rows.map(r => (
-            <tbody key={r.uuid} data-testid="snapshot">
-              <tr>
-                <td>{r.uuid}</td>
-                <td>{r.podName}</td>
-                <td>{r.environment}</td>
-                <td data-testid="snapshot-date">
-                  {(r.createdAt && r.createdAt.toLocaleString()) || "Unknown"}
-                </td>
-                <td>
-                  <Link href={r.link}>Framescope</Link>
-                </td>
-              </tr>
-            </tbody>
+            <SnapshotItem key={r.uuid} {...r} />
           ))}
           {this.props.rows.length === 0 && (
-            <tbody>
-              <tr className={styles.empty} data-testid="empty-message">
-                <td colSpan={5}>{this.props.emptyMessage}</td>
-              </tr>
-            </tbody>
+            <Empty emptyMessage={this.props.emptyMessage} />
           )}
         </table>
       </div>
