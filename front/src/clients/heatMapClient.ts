f3b8eef1b7d2b627f66f9e6a5f7ace56cbac33d7
Takahiro Okada <okada_tak@worksap.co.jp>
2018-11-15 18:31:18 +0900

    perf: drop polyfill to reduce js size

--- front/src/clients/heatMapClient.ts
+++ front/src/clients/heatMapClient.ts
@@ -2,47 +2,47 @@ import { FLAMESCOPE_API_BASE } from "../constants";
 
 export default function(fileName: string): Promise<IHeatMap> {
   const url = `${FLAMESCOPE_API_BASE}/?filename=${fileName}`;
-  return new Promise<IHeatMap>(async (resolve, reject) => {
-    let res;
-    try {
-      res = await fetch(url);
-    } catch (e) {
-      reject(e);
-      return;
-    }
-    if (!res.ok) {
-      reject(Error(`Failed to get from ${url}`));
-      return;
-    }
+  return new Promise<IHeatMap>((resolve, reject) => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          reject(Error(`Failed to get from ${url}`));
+          return;
+        }
 
-    const data = await res.json();
-    if (!data.hasOwnProperty("values")) {
-      reject(Error("Invalid data. Field `values` should exist."));
-      return;
-    }
-    if (!data.hasOwnProperty("columns")) {
-      reject(Error("Invalid data. Field `columns` should exist."));
-      return;
-    }
-    if (!data.hasOwnProperty("rows")) {
-      reject(Error("Invalid data. Field `rows` should exist."));
-    }
-    if (data.columns.length !== data.values.length) {
-      reject(Error("Invalid data. Column and Values are inconsistent."));
-    }
+        res.json().then(data => {
+          if (!data.hasOwnProperty("values")) {
+            reject(Error("Invalid data. Field `values` should exist."));
+            return;
+          }
+          if (!data.hasOwnProperty("columns")) {
+            reject(Error("Invalid data. Field `columns` should exist."));
+            return;
+          }
+          if (!data.hasOwnProperty("rows")) {
+            reject(Error("Invalid data. Field `rows` should exist."));
+          }
+          if (data.columns.length !== data.values.length) {
+            reject(Error("Invalid data. Column and Values are inconsistent."));
+          }
 
-    const values: number[] = [];
-    const rowLength: number = data.rows.length;
-    for (const x of data.values) {
-      if (rowLength !== x.length) {
-        reject(Error("Invalid data. Row and Values are inconsistent."));
-      }
-      Array.prototype.push.apply(values, x);
-    }
-    resolve({
-      maxValue: data.maxvalue,
-      values
-    });
+          const values: number[] = [];
+          const rowLength: number = data.rows.length;
+          for (const x of data.values) {
+            if (rowLength !== x.length) {
+              reject(Error("Invalid data. Row and Values are inconsistent."));
+            }
+            Array.prototype.push.apply(values, x);
+          }
+          resolve({
+            maxValue: data.maxvalue,
+            values
+          });
+        });
+      })
+      .catch(e => {
+        reject(e);
+      });
   });
 }
 
