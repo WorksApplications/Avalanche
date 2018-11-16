c366fb5738c773d97368efebc544d169a8edff3e
Takahiro Okada <okada_tak@worksap.co.jp>
2018-11-16 15:22:19 +0900

    refactor: use Promise chain instead of resolve,reject

--- front/src/clients/heatMapClient.ts
+++ front/src/clients/heatMapClient.ts
@@ -2,48 +2,40 @@ import { FLAMESCOPE_API_BASE } from "../constants";
 
 export default function(fileName: string): Promise<IHeatMap> {
   const url = `${FLAMESCOPE_API_BASE}/?filename=${fileName}`;
-  return new Promise<IHeatMap>((resolve, reject) => {
-    fetch(url)
-      .then(res => {
-        if (!res.ok) {
-          reject(Error(`Failed to get from ${url}`));
-          return;
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw Error(`Failed to get from ${url}`);
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (!data.hasOwnProperty("values")) {
+        throw Error("Invalid data. Field `values` should exist.");
+      }
+      if (!data.hasOwnProperty("columns")) {
+        throw Error("Invalid data. Field `columns` should exist.");
+      }
+      if (!data.hasOwnProperty("rows")) {
+        throw Error("Invalid data. Field `rows` should exist.");
+      }
+      if (data.columns.length !== data.values.length) {
+        throw Error("Invalid data. Column and Values are inconsistent.");
+      }
+
+      const values: number[] = [];
+      const rowLength: number = data.rows.length;
+      for (const x of data.values) {
+        if (rowLength !== x.length) {
+          throw Error("Invalid data. Row and Values are inconsistent.");
         }
-
-        res.json().then(data => {
-          if (!data.hasOwnProperty("values")) {
-            reject(Error("Invalid data. Field `values` should exist."));
-            return;
-          }
-          if (!data.hasOwnProperty("columns")) {
-            reject(Error("Invalid data. Field `columns` should exist."));
-            return;
-          }
-          if (!data.hasOwnProperty("rows")) {
-            reject(Error("Invalid data. Field `rows` should exist."));
-          }
-          if (data.columns.length !== data.values.length) {
-            reject(Error("Invalid data. Column and Values are inconsistent."));
-          }
-
-          const values: number[] = [];
-          const rowLength: number = data.rows.length;
-          for (const x of data.values) {
-            if (rowLength !== x.length) {
-              reject(Error("Invalid data. Row and Values are inconsistent."));
-            }
-            Array.prototype.push.apply(values, x);
-          }
-          resolve({
-            maxValue: data.maxvalue,
-            values
-          });
-        });
-      })
-      .catch(e => {
-        reject(e);
-      });
-  });
+        Array.prototype.push.apply(values, x);
+      }
+      return {
+        maxValue: data.maxvalue,
+        values
+      };
+    });
 }
 
 export interface IHeatMap {
