7b73fe6dd8c16aae75b2e0c9bd0d45c7ea42f6d3
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-11-21 16:33:59 +0900

    [stack] fmt

--- pkg/flamescope/stack/stack_test.go
+++ pkg/flamescope/stack/stack_test.go
@@ -5,12 +5,12 @@ import (
 )
 
 func example() []byte {
-    example := []byte(
-    `{"c": [
+	example := []byte(
+		`{"c": [
         {"c": [], "l": "", "n": "java", "v":1},
         {"c": [{"c": [], "l": "", "n": "Interpreter", "v": 1}], "l": "", "n": "kernel", "v":3}
      ], "v": 5, "l": "", "n": "java"}`)
-     return example
+	return example
 }
 
 func TestReadRaw(t *testing.T) {
@@ -18,9 +18,9 @@ func TestReadRaw(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-    if len(r.Children) != 2 {
+	if len(r.Children) != 2 {
 		t.Fatal(r)
-    }
+	}
 }
 
 func TestNewNameVec(t *testing.T) {
@@ -28,22 +28,21 @@ func TestNewNameVec(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-    t.Log(r)
-    m, rev := newNameVec(r)
-    if len(m) != len(rev) {
-        t.Fatalf("Dictionaries are inconsistent: %v, %v", m, rev)
-    }
-    if len(m) != 2 {
-        t.Fatalf("Unmatch with the example, map: %+v, rev: %+v", m, rev)
-    }
+	t.Log(r)
+	m, rev := newNameVec(r)
+	if len(m) != len(rev) {
+		t.Fatalf("Dictionaries are inconsistent: %v, %v", m, rev)
+	}
+	if len(m) != 2 {
+		t.Fatalf("Unmatch with the example, map: %+v, rev: %+v", m, rev)
+	}
 }
 
 func TestProcess(t *testing.T) {
 	r, _ := readRaw(example())
-    m, _:= newNameVec(r)
-    s := r.process(nil, &m)
-    if len(s.Children) != 2 {
+	m, _ := newNameVec(r)
+	s := r.process(nil, &m)
+	if len(s.Children) != 2 {
 		t.Fatal(s)
-    }
+	}
 }
-
