447065e38cfbe566f9b4119a5e4b471c529e4e2b
Iori Yoneji <yoneji_i@worksap.co.jp>
2018-11-22 21:52:57 +0900

    [stack] first implemantation of stack server

--- cmd/suspect/main.go
+++ cmd/suspect/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"log"
 	"flag"
 	"io/ioutil"
-	"log"
+	"net/http"
 
 	"git.paas.workslan/resource_optimization/dynamic_analysis/pkg/flamescope/stack"
 )
+
+type context struct {
+    collect string
+}
+
+func (ctx *context) getSnapshots(uuid string) {
+	http.Get(ctx.collect + "/snapshots/" + uuid)
+}
+
+func (ctx *context) analyze(w http.ResponseWriter, r *http.Request) {
+	log.Print(r.Proto, r.Method, ": ", r.URL, " | Header: ", r.Header)
+    if r.Method == "POST" {
+    }
+}
+
+func serve(at, collect string) {
+    ctx := context {collect}
+	http.HandleFunc("/", ctx.analyze)
+	log.Fatal(http.ListenAndServe(at, nil))
+}
 
 func main() {
 	log.SetPrefix("suspect:\t")
@@ -14,17 +35,24 @@ func main() {
 
 	sfn := flag.String("src", "test/stack", "file to read")
 	dfn := flag.String("dst", "test/filtered", "file to write")
+    at  := flag.String("http", "localhost:8080", "host:port")
+    collect := flag.String("collect", "http://collect:8080", "location for collect")
 	flag.Parse()
 	args := flag.Args()
 	log.Println(args)
-	data, err := ioutil.ReadFile(*sfn)
-	if err != nil {
-		panic(err)
-	}
-	fil, err := stack.FilterAndExport(data, 2)
-	if err != nil {
-		panic(err)
-	}
 
-	ioutil.WriteFile(*dfn, fil, 0644)
+    if at != nil {
+        serve(*at, *collect)
+    } else {
+        data, err := ioutil.ReadFile(*sfn)
+        if err != nil {
+            panic(err)
+        }
+        fil, err := stack.FilterAndExport(data, 2)
+        if err != nil {
+            panic(err)
+        }
+
+        ioutil.WriteFile(*dfn, fil, 0644)
+    }
 }
