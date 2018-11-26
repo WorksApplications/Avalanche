d2607b8c5fe42a18b17145adb6648895a92129c3
Takahiro Okada <okada_tak@worksap.co.jp>
2018-11-26 17:37:27 +0900

    feat: navigation is now in title bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,50 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import { Route, Router, Switch } from "react-router";
+import { NavLink } from "react-router-dom";
 import styles from "./App.scss";
-import TitleBar from "./containers/TitleBar";
+import { APP_NAME } from "./constants";
 import Toastr from "./containers/Toastr";
-import Workspace from "./containers/Workspace";
+import ConfigPage from "./pages/ConfigPage";
+import SnapshotPage from "./pages/SnapshotPage";
+import { history } from "./store";
 
 class App extends React.Component {
   public render() {
     return (
       <div className={styles.wrap}>
         <div className={styles.title}>
-          <TitleBar />
+          <span className={styles.brand}>{APP_NAME}</span>
+          <NavLink
+            exact={true}
+            activeClassName={styles.selected}
+            className={styles.viewItem}
+            to="/"
+          >
+            Snapshots
+          </NavLink>
+          <NavLink
+            activeClassName={styles.selected}
+            className={[styles.viewItem, styles.config].join(" ")}
+            to={"/config"}
+          >
+            Config
+          </NavLink>
         </div>
         <div className={styles.content}>
-          <Workspace />
+          <Router history={history}>
+            <main className={styles.main}>
+              <Switch>
+                <Route
+                  exact={true}
+                  component={SnapshotPage}
+                  path="/"
+                  default={true}
+                />
+                <Route component={ConfigPage} path="/config" />
+              </Switch>
+            </main>
+          </Router>
         </div>
         <Toastr />
       </div>
