fb6d22f67a472d38877b1c1954a1c8299cf81663
Takahiro Okada <okada_tak@worksap.co.jp>
2018-11-29 18:09:15 +0900

    feat: improve PerfCallTree style for many items

--- front/src/components/PerfCallTree/PerfCallTree.tsx
+++ front/src/components/PerfCallTree/PerfCallTree.tsx
@@ -17,6 +17,8 @@ export interface IProperty {
   onTargetChanged?(targetId: number): void;
 }
 
+type RenderingLabelLevel = "normal" | "classOnly" | "hiddenByDefault";
+
 const initialState = { targetId: null as number | null };
 type State = Readonly<typeof initialState>;
 
@@ -38,50 +40,98 @@ class PerfCallTree extends React.Component<IProperty, State> {
   public readonly state: State = initialState;
 
   public render() {
-    let elementToShowAsRoot =
-      this.state.targetId !== null && this.props.treeMap[this.state.targetId];
-    if (
-      elementToShowAsRoot &&
-      typeof elementToShowAsRoot.parentId !== "undefined"
-    ) {
-      elementToShowAsRoot = this.props.treeMap[elementToShowAsRoot.parentId];
+    if (this.state.targetId === null) {
+      return <ul className={styles.wrap} />;
     }
+    const targetElement = this.props.treeMap[this.state.targetId];
+    const parentElement =
+      typeof targetElement.parentId !== "undefined"
+        ? this.props.treeMap[targetElement.parentId]
+        : null;
+
+    const leftElements = parentElement
+      ? parentElement.childIds
+          .filter(i => i < targetElement.id)
+          .map(c => this.props.treeMap[c])
+      : [];
+    const rightElements = parentElement
+      ? parentElement.childIds
+          .filter(i => i > targetElement.id)
+          .map(c => this.props.treeMap[c])
+      : [];
+    const childElements = targetElement.childIds.map(
+      c => this.props.treeMap[c]
+    );
 
     return (
-      <div className={styles.wrap}>
-        {elementToShowAsRoot && this.renderTreeElement(elementToShowAsRoot, 3)}
-      </div>
+      <ul className={styles.wrap}>
+        <li>
+          {parentElement ? (
+            this.renderElement(parentElement, "normal")
+          ) : (
+            <div className={styles.parentPlaceholder} />
+          )}
+        </li>
+        <li>
+          <div className={styles.left}>
+            {this.renderElementArray(leftElements, true)}
+          </div>
+          <div className={styles.center}>
+            {this.renderElement(targetElement, "normal")}
+          </div>
+          <div className={styles.right}>
+            {this.renderElementArray(rightElements, true)}
+          </div>
+        </li>
+        <li>{this.renderElementArray(childElements, false)}</li>
+      </ul>
     );
   }
 
-  private renderTreeElement(element: ITreeElement, remainsDepth: number) {
+  private renderElement(element: ITreeElement, level: RenderingLabelLevel) {
+    const tokens = element.label.split("/");
+
+    const label =
+      level === "hiddenByDefault"
+        ? `${tokens[tokens.length - 1].substr(0, 5)}..`
+        : level === "classOnly"
+        ? tokens[tokens.length - 1]
+        : tokens.length > 3
+        ? `${tokens[0]}/${tokens[1]}/... ${tokens[tokens.length - 1]}`
+        : element.label;
+    const labelFull = element.label;
+
     return (
       <div className={styles.element}>
         <div
           className={[
             styles.label,
-            element.id === this.state.targetId ? styles.targetLabel : undefined
+            element.id === this.state.targetId ? styles.targetLabel : undefined,
+            level === "hiddenByDefault" ? styles.hiddenLabel : undefined
           ].join(" ")}
           onClick={this.onElementClick}
           data-elementid={element.id}
         >
-          {element.label}
+          {label}
+          <span className={styles.tooltip}>{labelFull}</span>
         </div>
-        <ul className={styles.childList}>
-          {remainsDepth > 1 &&
-            element.childIds.map(c => {
-              const ce = this.props.treeMap[c];
-              return (
-                ce &&
-                element.totalRatio * 0.1 < ce.totalRatio && ( // only shows 10%
-                  <li className={styles.child} key={ce.id}>
-                    {this.renderTreeElement(ce, remainsDepth - 1)}
-                  </li>
-                )
-              );
-            })}
-        </ul>
       </div>
+    );
+  }
+
+  private renderElementArray(elements: ITreeElement[], isHalfLength: boolean) {
+    const level: RenderingLabelLevel =
+      (isHalfLength && elements.length >= 3) || elements.length >= 5
+        ? "hiddenByDefault"
+        : "classOnly";
+    return (
+      <ul className={styles.childList}>
+        {elements.map(e => (
+          <li key={e.id} className={styles.child}>
+            {this.renderElement(e, level)}
+          </li>
+        ))}
+      </ul>
     );
   }
 
