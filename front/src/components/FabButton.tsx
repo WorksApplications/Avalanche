9551e2bf4343f1d6e7d26fafcb08a8657c6dd580
Takahiro Okada <okada_tak@worksap.co.jp>
2018-12-03 12:50:32 +0900

    refactor: replace tooltip with react-tooltip for FabButton

--- front/src/components/FabButton.tsx
+++ front/src/components/FabButton.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as ReactTooltip from "react-tooltip";
 import styles from "./FabButton.scss";
 
 interface IProperty {
@@ -8,11 +9,26 @@ interface IProperty {
 }
 
 export const FabButton: React.FunctionComponent<IProperty> = ({ ...props }) => (
-  <div className={styles.wrap} onClick={props.onClick}>
-    <div className={styles.innerWrap}>
-      <span className={styles.tooltip}>{props.tooltip}</span>
-      <i className={["material-icons", styles.icon].join(" ")}>{props.icon}</i>
+  <>
+    <div
+      className={styles.wrap}
+      onClick={props.onClick}
+      data-tip={props.tooltip}
+      data-for="fab"
+    >
+      <div className={styles.innerWrap}>
+        <i className={["material-icons", styles.icon].join(" ")}>
+          {props.icon}
+        </i>
+      </div>
     </div>
-  </div>
+    <ReactTooltip
+      id="fab"
+      effect="solid"
+      place="left"
+      aria-haspopup="true"
+      className={styles.tooltip}
+    />
+  </>
 );
 export default FabButton;
