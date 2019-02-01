614e07a9cac9735c0a94ca5b92e109e160948bb4
Iori Yoneji <yoneji_i@worksap.co.jp>
2019-02-01 14:21:22 +0900

    extract handles local fs as well

--- cmd/extract/main.go
+++ cmd/extract/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -18,6 +19,33 @@ import (
 	"github.com/xi2/xz"
 )
 
+func getLog(root *url.URL, resource string) (io.ReadCloser, error) {
+	// TODO: handle trailing slash
+	addr := root.String() + resource
+	if root.Scheme == "file" {
+		return os.Open(addr)
+	} else {
+		resp, err := http.Get(addr)
+		if err != nil {
+			return nil, err
+		}
+		return resp.Body, nil
+	}
+}
+
+func asTape(name string, in io.Reader) (io.Reader, error) {
+	switch filepath.Ext(filepath.Base(name)) {
+	case ".tar":
+		return in, nil
+	case ".gz":
+		return gzip.NewReader(in)
+	case ".xz":
+		return xz.NewReader(in, 0)
+	default:
+		return nil, fmt.Errorf("Unknown archive type: %s (ext: %s)", name, filepath.Ext(filepath.Base(name)))
+	}
+}
+
 func (c *config) handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, fmt.Sprintf("method %s is not supported", r.Method), 404)
@@ -25,44 +53,26 @@ func (c *config) handler(w http.ResponseWriter, r *http.Request) {
 	}
 	q := r.URL.Query()
 	resource := q.Get("resource")
-	resp, err := http.Get(c.logAt + resource)
+	ar, err := getLog(c.logAt, resource)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Fail to fetch archive %s (%v)", c.logAt+resource, err), 503)
+		http.Error(w, fmt.Sprintf("Fail to fetch archive %s (%v)", c.logAt.String()+resource, err), 503)
 		return
 	}
-	defer resp.Body.Close()
+	defer ar.Close()
 
-	files := make([]string, 0)
-	var tape io.Reader
-	switch filepath.Ext(filepath.Base(resource)) {
-	case ".tar":
-		tape = resp.Body
-	case ".gz":
-		tape, err = gzip.NewReader(resp.Body)
-		if err != nil {
-			msg := fmt.Sprintf("failed to decompress: %s (%v)", c.logAt+resource, err)
-			log.Print(msg)
-			http.Error(w, msg, 503)
-			return
-		}
-	case ".xz":
-		tape, err = xz.NewReader(resp.Body, 0)
-		if err != nil {
-			msg := fmt.Sprintf("failed to decompress: %s (%v)", c.logAt+resource, err)
-			log.Print(msg)
-			http.Error(w, msg, 503)
-			return
-		}
-	default:
-		msg := fmt.Sprintf("Unknown archive type: %s (%s)", c.logAt+resource, filepath.Ext(filepath.Base(resource)))
+	tape, err := asTape(resource, ar)
+	if err != nil {
+		msg := fmt.Sprintf("failed to decompress: %s (%v)", c.logAt.String()+resource, err)
 		log.Print(msg)
 		http.Error(w, msg, 503)
 		return
 	}
+
+	files := make([]string, 0)
 	tr := tar.NewReader(tape)
 	for hdr, err := tr.Next(); err != io.EOF; hdr, err = tr.Next() {
 		if err != nil {
-			msg := fmt.Sprintf("Fail to unarchive %s (%v)", c.logAt+resource, err)
+			msg := fmt.Sprintf("Fail to unarchive %s (%v)", c.logAt.String()+resource, err)
 			log.Print(msg)
 			http.Error(w, msg, 503)
 			return
@@ -105,20 +115,25 @@ func (c *config) handler(w http.ResponseWriter, r *http.Request) {
 
 type config struct {
 	mapRe *regexp.Regexp
-	logAt string
+	logAt *url.URL
 }
 
 func main() {
 	at := flag.String("serve", ":8080", "Server setting")
-	logAt := flag.String("logServer", "http://localhost", "Log server address")
+	logAt := flag.String("logServer", "http://localhost", "Log server address (http://) or volume location (file://)")
 	flag.Parse()
 
 	log.SetFlags(log.Lshortfile | log.Ltime | log.Ldate)
 
 	//perf_map_regex = re.compile(r'^perf[0-9]+\.map$')
+
+	url, err := url.Parse(*logAt)
+	if err != nil {
+		log.Fatal("The option \"logServer\" is malformed.", *logAt, err)
+	}
 	conf := config{
 		mapRe: regexp.MustCompile(`^perf[0-9]+\.map$`),
-		logAt: *logAt,
+		logAt: url,
 	}
 
 	http.HandleFunc("/", conf.handler)
